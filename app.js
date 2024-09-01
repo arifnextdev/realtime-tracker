88bbe97273528d1c40a00060eae6828369c13b4d
ariful171042 <arif171042@gmail.com>
2024-09-01 11:45:15 +0600

    f

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 
